ea4d8344ba81ee8e245b193247692e1fa438450a
dothinh5175-afk <dothinh5175@gmail.com>
2026-02-27 22:40:31 +0700

    Thinh update code moi lan 1

--- Migrations/20260227151538_AddIsStarredToNotification.Designer.cs
+++ Migrations/20260227151538_AddIsStarredToNotification.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using do_an_tot_nghiep.Models;
 
@@ -11,9 +12,11 @@ using do_an_tot_nghiep.Models;
 namespace do_an_tot_nghiep.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260227151538_AddIsStarredToNotification")]
+    partial class AddIsStarredToNotification
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20260227151538_AddIsStarredToNotification.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace do_an_tot_nghiep.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsStarredToNotification : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                schema: "dbo",
+                table: "Notifications",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsStarred",
+                schema: "dbo",
+                table: "Notifications",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ImageId",
+                schema: "dbo",
+                table: "Images",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint")
+                .Annotation("SqlServer:Identity", "1, 1")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsStarred",
+                schema: "dbo",
+                table: "Notifications");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                schema: "dbo",
+                table: "Notifications",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "ImageId",
+                schema: "dbo",
+                table: "Images",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+        }
+    }
+}
